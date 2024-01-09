4ce2b57b8e6587351136cd61a9e656c9200819ff
gabrielleescorel <gabrielleescorel@gmail.com>
2024-01-09 16:10:34 -0300

    Entrega de Atividade

--- cadastro-de-categorias.js
+++ cadastro-de-categorias.js
@@ -1,22 +1,37 @@
 
 
-        var imprime = document.getElementById("nomeCliente");
-        var bloco = document.getElementById("blocoDeNotas");
-        var contador = 1;
+var imprime = document.getElementById("nomeCliente");
+var bloco = document.getElementById("blocoDeNotas");
+var contador = 1;
+var categorias = [];
+var categoriasContainer = document.getElementById("categoriasContainer"); 
 
-        function salvar() {
-
-            if (imprime.value.trim() !== "") {
-                var novoParagrafo = document.createElement('p');
-                var texto = document.createTextNode("Codigo: " + contador + " Nome: " + imprime.value);
-                novoParagrafo.appendChild(texto);
-
-                bloco.appendChild(novoParagrafo);
-
-                contador++;
-
-                imprime.value = "";
-            } else {
-                alert("Digite!");
-            }
+function salvar() {
+    if (imprime.value.trim() !== "") {
+        
+        if (categorias.includes(imprime.value.trim())) {
+            alert("Cliente já cadastrado!");
+            return; 
         }
+
+        var novoParagrafo = document.createElement('p');
+        var texto = document.createTextNode("Codigo: " + contador + " Nome: " + imprime.value);
+        novoParagrafo.appendChild(texto);
+
+        bloco.appendChild(novoParagrafo);
+        categorias.push(imprime.value.trim()); 
+
+        contador++;
+        imprime.value = "";
+    } else {
+        alert("Digite!");
+    }
+}
+
+function exibirCategorias() {
+    
+    var categoriasElement = document.createElement('p');
+    categoriasElement.textContent = "Categorias cadastradas: " + categorias.join(', ');
+    categoriasContainer.innerHTML = "";
+    categoriasContainer.appendChild(categoriasElement);
+}
