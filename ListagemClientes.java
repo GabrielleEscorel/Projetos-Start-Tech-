0e66153778ee36cb735f0594f7138f740af3bc29
gabrielleescorel <gabrielleescorel@gmail.com>
2024-01-31 15:18:42 -0300

    Listegens Java

---
+++ ListagemClientes.java
@@ -0,0 +1,25 @@
+public class ListagemClientes {
+    public static void main(String[] args) {
+
+        String nome1 = "Jade";
+        int nascimentoJade = 1997;
+
+        String nome2 = "Claire";
+        int nascimentoClaire = 2000;
+
+        String linha = "―――――――――――――――";
+
+        System.out.println(">> Listagem dos Clientes");
+        System.out.println();
+        System.out.println(linha);
+        System.out.println("Nome: " + nome1);
+        System.out.println("Data de Nascimento: " + nascimentoJade);
+        System.out.println();
+        System.out.println(linha);
+        System.out.println("Nome: " + nome2);
+        System.out.println("Data de Nascimento: " + nascimentoClaire);
+
+    }
+}
+
+
---
+++ ListagemProdutos.java
@@ -0,0 +1,23 @@
+public class ListagemProdutos {
+    public static void main(String[] args) {
+
+        String nomeLivros = "Livros";
+        double precoLivros = 49.95;
+
+        String nomeCelular = "Celular";
+        double precoCelular = 1470.76;
+
+        String linha = "―――――――――――――――";
+
+        System.out.println(">> Listagem dos Produtos");
+        System.out.println();
+        System.out.println(linha);
+        System.out.println("Nome: " + nomeLivros);
+        System.out.println("Preço: R$ " + precoLivros);
+        System.out.println();
+        System.out.println(linha);
+        System.out.println("Nome: " + nomeCelular);
+        System.out.println("Preço: R$ " + precoCelular);
+
+    }
+}
